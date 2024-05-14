0d8992a3f48ce6d1ff71900acb78f911d99bedfa
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-05-14 16:54:22 +0300

    upd json

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+// src/components/App.jsx
+
+import { Routes, Route, Link } from "react-router-dom";
+
+
+
+export default function App() {
+ return 
+  }
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "modern-normalize";
-import App from './App.jsx'
+import App from "./components/App/App";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <App/>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
